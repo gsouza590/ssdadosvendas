60fe4a0246053285034e1a88c5607d9dcf06c37a
gsouza590 <gsouza590@gmail.com>
2023-05-10 15:36:34 -0300

    Modificando SMS Service e mudando variaveis no properties

--- backend/src/main/java/com/gabriel/sistemaVendas/Service/SmsService.java
+++ backend/src/main/java/com/gabriel/sistemaVendas/Service/SmsService.java
@@ -33,7 +33,7 @@ public class SmsService {
 		Sale sale = saleRepository.findById(id).get();
 		String date = sale.getDate().getMonth()+"/"+ sale.getDate().getYear();
 		String msg = "Vendedor "+ sale.getSellerName() + "foi destaque em " + date + " com um total de R$ "
-			+ String.format("%2f", sale.getAmount()) ;
+			+ String.format("%.2f", sale.getAmount()) ;
 		Twilio.init(twilioSid, twilioKey);
 
 		PhoneNumber to = new PhoneNumber(twilioPhoneTo);
